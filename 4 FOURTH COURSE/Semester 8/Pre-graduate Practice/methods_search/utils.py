e462d8ebc7006538a36761942adae05a8688a4f0
egorywka <88832718+egorbeckish@users.noreply.github.com>
2025-03-01 15:49:41 +0700

    Delete 4 FOURTH COURSE/Semester 8/Pre-graduate Practice directory

--- 4 FOURTH COURSE/Semester 8/Pre-graduate Practice/methods_search/utils.py
+++
@@ -1,8 +0,0 @@
-from library import *
-
-
-def del_space(document: list) -> list:
-	while '' in document:
-		document.pop(document.index(''))
-
-	return document
--- 4 FOURTH COURSE/Semester 8/Pre-graduate Practice/parse word table/self parse/library.py
+++
@@ -1,2 +0,0 @@
-import docx
-from docx import Document
