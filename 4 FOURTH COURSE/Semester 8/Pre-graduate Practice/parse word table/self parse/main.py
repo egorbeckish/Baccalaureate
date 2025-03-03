26fd050a6356520cf8bfa5c08bbf2bae05e68fc8
egorywka <88832718+egorbeckish@users.noreply.github.com>
2025-03-03 09:05:23 +0000

    commit

--- 4 FOURTH COURSE/Semester 8/Pre-graduate Practice/parse word table/self parse/main.py
+++ 4 FOURTH COURSE/Semester 8/Pre-graduate Practice/parse word table/self parse/main.py
@@ -1,10 +1,10 @@
 from utils import *
 
 
-docx: Document = Document('doc.docx')
+docx: Document = Document('тест босс таблицы.docx')
 tables: list = docx.tables
 # print(tables)
-table = tables[3]
+table = tables[0]
 
 headers: list[str] = [cell.text for cell in table.rows[0].cells]
 data: list[str] = [[cell.text for cell in row.cells] for row in table.rows[1:]]
@@ -19,11 +19,11 @@ data: list[str] = data[1:] if data.count(data[0]) > 1 else data
 #     )
 # )
 
-# write_table(
-#     tabulate(
-#         data, 
-#         headers=headers, 
-#         tablefmt="simple_grid", 
-#         showindex=False
-#     ) + '\n\n'
-# )
+write_table(
+    tabulate(
+        data, 
+        headers=headers, 
+        tablefmt="simple_grid", 
+        showindex=False
+    ) + '\n\n'
+)
