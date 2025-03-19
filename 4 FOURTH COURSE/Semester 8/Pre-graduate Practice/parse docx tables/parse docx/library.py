00fa51d65a577f663aa042cb99cf35ea00f7f706
egorywka <88832718+egorbeckish@users.noreply.github.com>
2025-03-19 13:40:36 +0700

    commit

--- 4 FOURTH COURSE/Semester 8/Pre-graduate Practice/parse docx tables/parse docx/library.py
+++ 4 FOURTH COURSE/Semester 8/Pre-graduate Practice/parse docx tables/parse docx/library.py
@@ -1,21 +1,12 @@
-try:
-    import docx
-    from docx import Document
-    from docx.oxml.text.paragraph import CT_P
-    from docx.oxml.table import CT_Tbl
-    
-    from tabulate import tabulate
-    
-    import os
-    
-    import regex
-    
-    from pdf2docx import Converter, parse
+import docx
+from docx import Document
+from docx.oxml.text.paragraph import CT_P
+from docx.oxml.table import CT_Tbl
 
-    os.system('clear; python main.py')
+from tabulate import tabulate
 
-except ImportError:
-    import os
-    
-    
-    os.system('python3 -m pip install --upgrade pip; pip install -r requirements.txt; clear; python library.py; clear; python main.py')
+import os
+
+import regex
+
+from pdf2docx import Converter, parse
